f4140a991a1b02e6357a2003e0bd629f771628bc
Rain-YYC <18132110008@163.com>
2026-05-03 02:35:29 +0800

    更新网站副标题为“一个网站”

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "Rain.Wheat",
-	subtitle: "One demo website",
+	subtitle: "One website",
 	siteURL: "https://rainwheat.cn/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
